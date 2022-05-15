441c815a84935d77e5dbbd012d899a63a50db5a6
Jesse Eisses <jesse.eisses@gmail.com>
2022-05-15 09:45:44 +0200

    Add an nrepl server to production deployment
    so we can debug jobs live

--- src/nosana_node/main.clj
+++ src/nosana_node/main.clj
@@ -6,9 +6,18 @@
             nos.system
             nos.module.http
             [duct.core :as duct]
+            [integrant.core :as ig]
+            [nrepl.server :as nrepl-server]
+            [cider.nrepl :refer (cider-nrepl-handler)]            )
   (:gen-class))
 
 (duct/load-hierarchy)
+
+(defmethod ig/init-key :nosana-node/nrepl-server [_ {:keys [port] :or {port 7888}}]
+  (nrepl-server/start-server :port port :handler cider-nrepl-handler))
+
+(defmethod ig/halt-key! :nosana-node/nrepl-server [_ {:keys [server-socket]}]
+  (.close server-socket))
 
 (defn -main [& args]
   (println "Starting system")
