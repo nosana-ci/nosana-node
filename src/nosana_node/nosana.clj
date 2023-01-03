21a6b7af8ceeadac6be15f54c81240b3d5967336
Jesse Eisses <jesse.eisses@gmail.com>
2023-01-03 15:57:18 +0100

    Set default secrest endpoint to devnet

--- src/nosana_node/nosana.clj
+++ src/nosana_node/nosana.clj
@@ -201,7 +201,7 @@ Running Nosana Node %s
         nft-ata      (sol/get-ata signer-pub nft)]
     {:network           network
      :signer            signer
-     :secrets-endpoint  "http://localhost:4124"
+     :secrets-endpoint  "https://secrets.k8s.dev.nos.ci"
      :pinata-jwt        (:pinata-jwt vault)
      :ipfs-url          (:ipfs-url vault)
      :market            market-pub
