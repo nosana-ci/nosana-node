844bea4a7394f0274de933b77ef03a791e4484dc
Jesse Eisses <jesse.eisses@gmail.com>
2023-01-04 00:56:12 +0100

    Refactor secret manager fx to :nos/secret

--- src/nosana_node/secrets.clj
+++ src/nosana_node/secrets.clj
@@ -6,21 +6,25 @@
             [nos.core :as flow]
             [cheshire.core :as json]))
 
-(defn- make-login-payload [{:keys [signer] :as conf} job-addr]
-  (let [ts  (quot (System/currentTimeMillis) 1000)
-        msg (-> (str "nosana_secret_" ts) (.getBytes "UTF-8"))
-        sig (sol/sign msg signer)
+(defn- make-login-payload
+  "Creates the HTTP payload for a login to the secrets engine.
+  If `job-addr` is set this payload gives you access to the secrets of
+  the job."
+  [{:keys [signer] :as conf} job-addr]
+  (let [ts      (quot (System/currentTimeMillis) 1000)
+        msg     (-> (str "nosana_secret_" ts) (.getBytes "UTF-8"))
+        sig     (sol/sign msg signer)
         payload (cond-> {:address   (.toString (:address conf))
                          :signature  (util/base58 sig)
                          :timestamp ts }
-                  job-addr (assoc :job job-addr))] 
+                  job-addr (assoc :job job-addr))]
     payload))
 
-(defn login 
+(defn login
+  "Retrieves a JWT token to authenticate with the secrets manager."
   ([conf] (login conf nil))
-  ( [{:keys [secrets-endpoint] :as conf} job-addr]
+  ([{:keys [secrets-endpoint] :as conf} job-addr]
    (let [payload (make-login-payload conf job-addr)]
-    ;;  (prn payload)
      (->
       (http/post (str secrets-endpoint "/login")
                  {:form-params payload
@@ -30,7 +34,7 @@
       :token))))
 
 (defn set-secrets
-  "Set a secrets map in the nosana secret proxy."
+  "Sets a secrets map in the nosana secret proxy."
   [{:keys [secrets-endpoint] :as conf} secrets]
   (let [jwt (login conf)]
     (http/post (str secrets-endpoint "/secrets")
@@ -39,30 +43,31 @@
                 :headers {"Authorization" jwt}
                 :accept :json})))
 
-(defn get-secrets [{:keys [secrets-endpoint] :as conf} jwt]
-  (try
-    (->
-     (http/get (str secrets-endpoint "/secrets")
-               {:headers {"Authorization" jwt}})
-     :body
-     (json/decode false))
-    (catch Exception e
-      ;; (prn "Error fetching secerts" (ex-data e))
-      (throw (ex-info "Error fetching secrets" (ex-data e))))))
+(defn get-secrets
+  ([conf jwt] (get-secrets conf jwt false))
+  ([{:keys [secrets-endpoint] :as conf} jwt keywordize?]
+   (try
+     (->
+      (http/get (str secrets-endpoint "/secrets")
+                {:headers {"Authorization" jwt}})
+      :body
+      (json/decode keywordize?))
+     (catch Exception e
+       ;; (prn "Error fetching secerts" (ex-data e))
+       (throw (ex-info "Error fetching secrets" (ex-data e)))))))
 
-(derive :nosana-node.core/secret ::flow/ref)
-(defmethod flow/ref-val :nosana-node.core/secret
-  [[_ endpoint value] flow-state vault]
-  ;; (prn "========== getting a sccret" endpoint value)
+(derive :nos/secret ::flow/ref)
+(defmethod flow/ref-val :nos/secret
+  [[_ endpoint value keywordize?] flow-state vault]
   (let [account (nos/get-signer-key vault)
         conf    {:signer           account
                  :secrets-endpoint endpoint
                  :address          (.getPublicKey account)}
         jwt     (login conf (:input/job-addr flow-state))
-        secrets (get-secrets conf jwt)]
-    ;; (prn secrets )
-    (if (contains? secrets value) 
-      (get secrets value) 
+        secrets (get-secrets conf jwt false)]
+    (if (contains? secrets value)
+      (cond-> (get secrets value)
+        keywordize? clojure.walk/keywordize-keys)
       (throw (ex-info "Could not find secret" {:value value})))))
 
 ;; nosana-node.core/secret can be used to retreive values from the secret engine
@@ -70,4 +75,4 @@
 #_(flow/run-flow!
    (:nos/flow-engine system)
    (flow/build
-    {:ops [{:op :prn :args [[:nosana-node.core/secret "http://localhost:4124" "jesse"]]}]}));
+    {:ops [{:op :nos/prn :args [[:nosana/secret "http://localhost:4124" "testing"]]}]}));
