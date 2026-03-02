8bc3e4b77e168b63b5d3c8c0782501db32d34665
Matthew Hammond <ultramatt96@hotmail.co.uk>
2026-03-02 05:55:55 +0000

    add dev run options

---
+++ docker-run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Build the Docker image
+docker build -t nosana_node:latest -f Dockerfile .
+
+# Create the network if it doesn't exist
+docker network create --driver bridge --subnet 192.168.101.0/24 --gateway 192.168.101.1 NOSANA_GATEWAY 2>/dev/null || true
+
+# Run the container with file watching
+docker run -it --rm \
+  --name nosana-node \
+  --network NOSANA_GATEWAY \
+  --env APP_ENV=development \
+  -v ~/.nosana/:/root/.nosana/ \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v "$(pwd)"/src:/nosana/src \
+  -v "$(pwd)"/package.json:/nosana/package.json \
+  -v "$(pwd)"/tsconfig.json:/nosana/tsconfig.json \
+  nosana_node:latest \
+  $@
