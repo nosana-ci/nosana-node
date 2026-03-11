f33b9b630785427e550ed87d97b5c1340383e4e4
Matthew Hammond <ultramatt96@hotmail.co.uk>
2026-03-11 09:41:30 +0000

    Add DNS caching and ipv4 first

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 #!/usr/bin/env -S node --no-warnings
 /// <reference path="./global.d.ts" />
 
+import http from 'http';
+import https from 'https';
+import { setDefaultResultOrder } from 'dns';
+import CacheableLookup from 'cacheable-lookup';
+
 import { pkg } from './static/index.js';
 import { startCLI } from './cli/index.js';
 
 const VERSION: string = pkg.version;
+
+setDefaultResultOrder('ipv4first');
+
+const cacheable = new CacheableLookup();
+cacheable.install(http.globalAgent);
+cacheable.install(https.globalAgent);
 
 startCLI(VERSION);
 
