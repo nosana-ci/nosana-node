8d1b685ebb5d4824552aa193d761ac4030595f03
Matthew Hammond <ultramatt96@hotmail.co.uk>
2026-08-06 22:48:25 +0900

    bump resource manager

--- src/NodeManager/node/resource/definition/index.ts
+++ src/NodeManager/node/resource/definition/index.ts
@@ -1,4 +1,4 @@
 export const s3HelperImage =
-  'registry.hub.docker.com/nosana/resource-manager:v1.1.15';
+  'registry.hub.docker.com/nosana/resource-manager:v1.1.16';
 
 export const nosanaBucket = 'https://models.nosana.io';
