b7391971068a2187f60b6fcf354ba41c76d9b737
Matthew Hammond <ultramatt96@hotmail.co.uk>
2026-08-07 00:57:59 +0900

    bump resource manager to reset retry counter after reconnection

--- src/NodeManager/node/resource/definition/index.ts
+++ src/NodeManager/node/resource/definition/index.ts
@@ -1,4 +1,4 @@
 export const s3HelperImage =
-  'registry.hub.docker.com/nosana/resource-manager:v1.1.16';
+  'registry.hub.docker.com/nosana/resource-manager:v1.1.17';
 
 export const nosanaBucket = 'https://models.nosana.io';
